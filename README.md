2a2dcc666b2b66b23323f7342a8321e6fa14c068
kentshenlim <kentshen_lim24@hotmail.com>
2023-10-15 17:08:19 +0800

    Display home

---
+++ README.md
@@ -0,0 +1,8 @@
+# Message Category
+
+## TODO
+
+- TODO: pagination
+- TODO: smart date
+- TODO: set the svg string as virtual, no need store into DB
+- TODO: most recent
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+
 const router = express.Router();
 
+const jdenticon = require('jdenticon');
+
+const messages = [
+  {
+    text: 'Hi there',
+    user: 'Amando',
+    added: new Date(),
+    img: jdenticon.toSvg('Amando', 200),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+    img: jdenticon.toSvg('Charles', 200),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Mini Messageboard', msgArr: messages });
 });
 
 module.exports = router;
